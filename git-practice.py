5251410d1688b9e7b4dc077afa3c0e2fc6150632
Kati Canton <alienvolution@gmail.com>
2023-09-07 18:38:51 +0000

    made delete feature

--- git-practice.py
+++ git-practice.py
@@ -4,9 +4,31 @@ def addItem(item):
    toDoList.append(item)
    return toDoList
 
-userAns = input("Do you want to add to your list or quit? A/Q")
+def deleteItem(item):
+   newToDo = []
+   toDoDup = []
+   for i in toDoList:
+      newToDo.append(i)
+      print("There's no duplicates!")
+   else:
+      toDoDup.append(i)
+      print("we have found duplicates!" + toDoDup)
+   
+   toDoList.remove(item)
+   
+
+userAns = input("Do you want to add to your list ,delete an item, or quit? A/D/Q")
+
 while userAns == "A":
    item = input("What item do you want to add?")
    addItem(item)
-   userAns = input("Do you want to add to your list or quit? A/Q")
+   userAns = input("Do you want to add to your list, delete an item, or quit? A/D/Q")
    print(toDoList)
+   if userAns == "D":
+     item = input("What would you like to delete?")
+     deleteItem(item)
+     userAns = input("Do you want to add to your list, delete an item, or quit? A/D/Q")
+   print(toDoList)
+
+
+
